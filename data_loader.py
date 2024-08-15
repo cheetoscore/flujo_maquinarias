ca2eab39d821aaafab508e5a663ca41dd37c28d2
cheetoscore <134351739+cheetoscore@users.noreply.github.com>
2024-08-15 09:08:37 -0500

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,20 @@
 import pandas as pd
-from sqlalchemy import create_engine
 import streamlit as st
 from db_config import get_engine
 
 # Obtener el motor de la base de datos
 engine = get_engine()
 
-# Funciones para obtener los datos necesarios con caché
 @st.cache_data(ttl=600)
 def get_egresos(start_date, end_date):
     query = f"""
-    SELECT "FECHADOC", "TOTAL", "COD_EQUIPO", "NOMBREPROYECTO", "GLOSA"
+    SELECT "FECHADOC", "TOTAL", "COD_EQUIPO", "NOMBREPROYECTO", "GLOSA", "SOCIO DE NEGOCIOS"
     FROM maquinaria_out
     WHERE "FECHADOC" BETWEEN '{start_date}' AND '{end_date}'
     """
     try:
-        return pd.read_sql(query, engine)
+        with engine.connect() as connection:
+            return pd.read_sql(query, connection)
     except Exception as e:
         st.error(f"Error al obtener egresos: {e}")
         return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
@@ -28,7 +27,8 @@ def get_deudas(start_date, end_date):
     WHERE "Fecha de vencimiento" BETWEEN '{start_date}' AND '{end_date}'
     """
     try:
-        return pd.read_sql(query, engine)
+        with engine.connect() as connection:
+            return pd.read_sql(query, connection)
     except Exception as e:
         st.error(f"Error al obtener deudas: {e}")
         return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
@@ -41,7 +41,8 @@ def get_ingresos(start_date, end_date):
     WHERE "Fecha" BETWEEN '{start_date}' AND '{end_date}'
     """
     try:
-        return pd.read_sql(query, engine)
+        with engine.connect() as connection:
+            return pd.read_sql(query, connection)
     except Exception as e:
         st.error(f"Error al obtener ingresos: {e}")
         return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
@@ -53,7 +54,8 @@ def get_proyectos():
     FROM proyecto
     """
     try:
-        return pd.read_sql(query, engine)
+        with engine.connect() as connection:
+            return pd.read_sql(query, connection)
     except Exception as e:
         st.error(f"Error al obtener proyectos: {e}")
         return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
@@ -65,8 +67,10 @@ def get_lista_equipos():
     FROM lista_equipos
     """
     try:
-        return pd.read_sql(query, engine)
+        with engine.connect() as connection:
+            return pd.read_sql(query, connection)
     except Exception as e:
         st.error(f"Error al obtener lista de equipos: {e}")
         return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
+
 
